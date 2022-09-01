3ea2b74d9ed66395adfb7480c40c7c62fee82d7a
Cristian Trapero <critramo@gmail.com>
2022-09-01 22:31:11 +0200

    add wifi version project

--- app/src/androidTest/java/com/cristiantrapero/loragram/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/cristiantrapero/loragram/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.cristiantrapero.tfm;
+package com.cristiantrapero.loragram;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.cristiantrapero.tfm", appContext.getPackageName());
+        assertEquals("com.cristiantrapero.loragram", appContext.getPackageName());
     }
 }
