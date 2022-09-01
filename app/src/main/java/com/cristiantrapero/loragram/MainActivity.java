41c07e97335440372bf5eb4f7c990bd15c56346f
Cristian Trapero <critramo@gmail.com>
2022-09-01 22:51:42 +0200

    scan interface

--- app/src/main/java/com/cristiantrapero/loragram/MainActivity.java
+++ app/src/main/java/com/cristiantrapero/loragram/MainActivity.java
@@ -2,13 +2,21 @@ package com.cristiantrapero.loragram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
+import com.cristiantrapero.tfm.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
     }
 }
